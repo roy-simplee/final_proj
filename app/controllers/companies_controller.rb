277452cde0fc4a3d86d5187d161403f06129bd75
Saba <roy@simplee.com>
2017-06-13 16:42:04 +0300

    temp

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,6 +1,6 @@
 class CompaniesController < ApplicationController
   def index
-    @companies = Company.order()
+    @companies = Company.order(:rating).paginate(page: params[:page], per_page: 10)
   end
 
   def show
